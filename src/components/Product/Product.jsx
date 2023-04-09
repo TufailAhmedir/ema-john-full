9e2eeecfa5a7c443a0db18a396f18420ca44266e
TufailAhmedir <tufailahmed1589@gmail.com>
2023-04-09 17:08:35 +0600

    cards ready

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import './Product.css';
 const Product = (props) => {
     // console.log(props.product);
-    const {img, name, seller, quantity, price} = props.product;
+    const {img, name, seller, quantity, price, ratings} = props.product;
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='product-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price:$ {price}</p>
+            <p> Manufacturer: {seller}</p>
+            <p>Price:$ {price}</p>
+            <p>Rating:{ratings} Stars </p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
